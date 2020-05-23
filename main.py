d1ebbb75459e4eeedd864ed79794ce16913fe535
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-23 17:57:19 +0300

    d = c.split(' | ')

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ with open('recipes.txt', 'r') as file:
 	print(file.read())
 
 cook_book = {'dish': [{'ingredient_name': [], 'quantity': [], 'measure': []}]}
-a = {'ingredient_name': [], 'quantity': [], 'measure': []}
-b = '666'
-
-a['quantity'].append(b)
+a = {'ingredient_name': None, 'quantity': None, 'measure': None}
+# b = '666'
+# a['quantity'].append(b)
 
 print(a)
 
+c = 'Винный уксус | 1 | ст.л'
+
+d = c.split(' | ')
+print(d)
+a['ingredient_name'] = d[0]
+a['quantity'] = d[1]
+a['measure'] = d[2]
+
+print(a)
