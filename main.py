856fd8bcbe81b4c3b89e69a7fa87f2f23b78ac5d
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-23 17:44:40 +0300

    coding 1251

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 with open('recipes.txt', 'r') as file:
 	print(file.read())
+
+cook_book = {'dish': [{'ingredient_name': [], 'quantity': [], 'measure': []}]}
+a = {'ingredient_name': [], 'quantity': [], 'measure': []}
+b = '666'
+
+a['quantity'].append(b)
+
+print(a)
+
